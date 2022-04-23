858fb9195f501a4be811449b8d6f17822177fb74
rpkrupali1 <rpkrupali@gmail.com>
2022-04-23 12:44:39 -0500

    login mutuation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,31 @@
+const { AuthenticationError } = require("apollo-server-express");
 const { User, Book } = require("../models");
 
 const resolvers = {
   Query: {
     users: async () => {
       return User.find();
+    },
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      return user;
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      return user;
     },
   },
 };
