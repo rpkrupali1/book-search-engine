c023f2c25d225534903133917ca8a5e60f78a82a
rpkrupali1 <rpkrupali@gmail.com>
2022-04-28 07:16:50 -0500

    front end mutation and query

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -19,28 +19,15 @@ export const ADD_USER = gql`
       user {
         _id
         username
+        email
       }
     }
   }
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook(
-    $bookId: ID
-    $authors: [String]
-    $description: String
-    $image: String
-    $link: String
-    $title: String
-  ) {
-    saveBook(
-      bookId: $bookId
-      authors: $authors
-      description: $description
-      image: $image
-      link: $link
-      title: $title
-    ) {
+  mutation saveBook($input: SaveBook!) {
+    saveBook(input: $input) {
       _id
       email
       username
