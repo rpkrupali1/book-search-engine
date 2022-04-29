74283adb038cc37b9302cd547a50713f65877feb
rpkrupali1 <rpkrupali@gmail.com>
2022-04-29 06:25:57 -0500

    cleared the linting errors on saved book

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -10,7 +10,7 @@ import {
 } from "react-bootstrap";
 
 import Auth from "../utils/auth";
-import { saveBook, searchGoogleBooks } from "../utils/API";
+import { searchGoogleBooks } from "../utils/API";
 import { saveBookIds, getSavedBookIds } from "../utils/localStorage";
 import { useMutation, useQuery } from "@apollo/client";
 import { SAVE_BOOK } from "../utils/mutations";
@@ -70,14 +70,14 @@ const SearchBooks = () => {
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
-    console.log(bookToSave);
+    // console.log(bookToSave);
 
-    // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // // get token
+    // const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+    // if (!token) {
+    //   return false;
+    // }
 
     try {
       await saveBook({
